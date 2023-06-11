ee2fe14aaee19c212442d4cfb71c4350d09c56d8
buddhi444 <fiatmillionaire@yahoo.com>
2023-06-11 18:47:05 +0530

    new

--- test_calculator.py
+++ test_calculator.py
@@ -6,12 +6,20 @@
         print("3 squared was not 9")"""
 import pytest
 from calculator2 import square
-def test_square():
+def test_positive():
     assert square(2)==4
     assert square(3)==9
+
+def test_negative():
     assert square(-2)==4
     assert square(-3)==9
+
+def test_zero():
     assert square(0)==0
+
+def test_str():
+    with pytest.raises(TypeError):
+        square("cat")
 
 
 
